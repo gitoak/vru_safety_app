4ff1d19991a98ad287c62a1ed3b367ede085d913
gitoak <74053774+gitoak@users.noreply.github.com>
2025-05-23 19:19:05 +0200

    Add AccessibilityTools wrapper to MaterialApp for improved accessibility

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:accessibility_tools/accessibility_tools.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -105,12 +106,14 @@ class _MyAppState extends State<MyApp> {
     if (_showOnboarding) {
       return MaterialApp(
         theme: appTheme,
+        builder: (context, child) => AccessibilityTools(child: child),
         home: OnboardingPage(onFinish: _handleOnboardingFinish),
       );
     }
 
     return MaterialApp.router(
       theme: appTheme,
+      builder: (context, child) => AccessibilityTools(child: child),
       routerDelegate: _routerDelegate,
       routeInformationParser: _routeInformationParser,
       backButtonDispatcher: RootBackButtonDispatcher(),
