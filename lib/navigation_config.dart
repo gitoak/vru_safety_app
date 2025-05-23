f9f306b73c3bce53be7759affa427565ce064d06
gitoak <74053774+gitoak@users.noreply.github.com>
2025-05-23 11:30:04 +0200

    navigation parity

--- lib/navigation_config.dart
+++ lib/navigation_config.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/graph_hopper_poc_page.dart';
 import 'pages/home_page.dart';
 import 'pages/sandbox_page.dart';
 import 'pages/settings_page.dart';
@@ -55,6 +56,14 @@ final List<NavScreenConfig> navScreens = [
     label: 'OSM POC',
     icon: Icons.map,
     builder: () => const OsmPocPage(),
+    inNavBar: false,
+  ),
+  // SandboxGraphhopperPage
+  NavScreenConfig(
+    route: '/graphhopper-poc',
+    label: 'GraphHopper POC',
+    icon: Icons.map,
+    builder: () => const GraphHopperPocPage(),
     inNavBar: false,
   ),
 ];
--- lib/pages/sandbox_page.dart
+++ lib/pages/sandbox_page.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:vru_safety_app/pages/sandbox_graphhopper_page.dart';
 import 'osm_poc_page.dart';
+import '../navigation_config.dart';
+import '../bloc/nav_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SandboxPage extends StatelessWidget {
   const SandboxPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox Page'),
+        leading: IconButton(
+          icon: const Icon(Icons.home),
+          onPressed: () {
+            // Use Bloc navigation to go to Home (or any other route from navigation_config.dart)
+            final homeIndex = navScreens.indexWhere((s) => s.route == '/');
+            if (homeIndex != -1) {
+              context.read<NavBloc>().add(NavTo(AppPage.values[homeIndex]));
+            }
+          },
+        ),
+      ),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
