2b7d03b6dddcedd4a26374d1ae3c6a7a7c5f7ba2
SpaceDev <75725016+TheSpaceDev@users.noreply.github.com>
2025-05-23 11:32:12 +0200

    Reapply "Merge branch 'main' of https://github.com/gitoak/vru_safety_app"
    This reverts commit 07f4f0b2663e8c9e8bd9b9afbfcca770ba515428.

--- lib/pages/sandbox_page.dart
+++ lib/pages/sandbox_page.dart
@@ -1,41 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:vru_safety_app/pages/sandbox_graphhopper_page.dart';
 import 'osm_poc_page.dart';
+import 'graph_hopper_poc_page.dart';
 
 class SandboxPage extends StatelessWidget {
-  const SandboxPage({Key? key}) : super(key: key);
+  const SandboxPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Sandbox Page'),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to OSM POC page
-                Navigator.of(
-                  context,
-                ).push(MaterialPageRoute(builder: (_) => const OsmPocPage()));
-              },
-              child: const Text('Go to OSM POC Page'),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => const SandboxGraphhopperPage(),
-                  ),
-                );
-              },
-              child: const Text('Go to GraphHopper Sandbox'),
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Sandbox Page'),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => const OsmPocPage()),
+              );
+            },
+            child: const Text('Go to OSM POC Page'),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (_) => const SandboxGraphhopperPage()),
+              );
+            },
+            child: const Text('Go to GraphHopper Sandbox'),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => const GraphHopperPocPage()),
+              );
+            },
+            child: const Text('Go to Graph Hopper POC Page'),
+          ),
+        ],
       ),
     );
   }
